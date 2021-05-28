d27ea6b2f5a8736f2d13794738fd2cc6dbe45407
Davidhzk2 <dherrera035@misena.edu.co>
2021-05-28 18:47:50 -0500

    Carpeta middleware y archivo con la autenticacion

---
+++ backend/middleware/auth.js
@@ -0,0 +1,23 @@
+// importar modulos
+const jwt = require("jsonwebtoken");
+
+// logica 
+const auth = (req, res, next)=>{
+    //revisar el header 
+    let jwtToken = req.header("Authorization");
+    if(!jwtToken) return res.status(401).send({Message: "Autorizacion rechazada: no hay token"});
+    // separar el jwt del header 
+    jwtToken  = jwtToken.spilt(" ")[1];
+    if(!jwtToken) return res.status(401).send({Message: "Autorizacion rechazada: no hay token"});
+    // verificar token y capturar errores
+    try {
+        const payload = jwt.verify(jwtToken, "");
+        req.user = payload;
+        next();
+    } catch (error) {
+        return res.status(401).send({Message: "Autorizacion rechazada: token no valido"});
+    }
+}
+
+// exportar modulo
+module.exports = auth;
---
+++ backend/routes/auth.js
@@ -0,0 +1,11 @@
+// importar modulos
+const express = require("express");
+const router = express.Router();
+
+//Logica 
+router.post("/login/", async (req, res)=>{
+
+});
+
+// exportar modulo
+module.exports = router;

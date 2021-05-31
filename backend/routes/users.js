7412a2f12557c2108f074bcaa380519963bad28d
AlejaB-droid <alejandra.barrios.1900@gmail.com>
2021-05-31 10:04:21 -0500

    updated change of status

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -11,8 +11,15 @@ router.put("/cambiarStatus", Auth, async(req, res) => {
     const user = await User.findById(req.user._id);
     if(!user) return res.status(400).send("El usuario no existe");
 
+    const hash = await bcrypt.hash(req.body.password, 10)
     user = await User.findByIdAndUpdate(req.body._id, {
-        status: req.body.status
+    rol: req.body.rol,
+    name: req.body.name, 
+    lastName: req.body.lastName,
+    email: req.body.email, 
+    password:hash,
+    phone: req.body.phone,
+    status: req.body.status
     });
     return res.status(200).send({user});
 });

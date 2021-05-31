9c47134ef2dc090a16d2738f84af9f120c111aaa
Davidhzk2 <dherrera035@misena.edu.co>
2021-05-31 18:23:16 -0500

    update post

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -5,4 +5,22 @@ const User = require("../models/users");
 const Post = require("../models/post");
 
 
+
+router.put("/updatePost",Auth, async (req, res) =>{
+    const user = await User.findById(req.user.id);
+    if(!user) return res.status(401).send({Message: "No se encontro el usuario"});
+
+    const post = await Post.findByIdAndUpdate(req.body._id,{
+        userId: req.user.id,
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        status: req.body.status
+    }); 
+
+    if(!post) return res.status(401).send({Message: "No se pudo avtualizar la publicación"});
+    return res.status(200).send({post});
+});
+
+
 module.exports = router;

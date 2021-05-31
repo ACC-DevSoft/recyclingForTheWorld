d556f58b54e5dc4d21b56736d57c9a0e4522354a
AlejaB-droid <alejandra.barrios.1900@gmail.com>
2021-05-31 09:35:25 -0500

    chande status of the user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,21 @@
 // importar modulos
 const express = require("express");
 const router = express.Router();
+const User = require("../models/users");
+const Auth = require("../middleware/auth");
 
 // logica
 
+// eliminar usuario
+router.post("/cambiarStatus", Auth, async(req, res) => {
+    const user = await User.findById(req.user._id);
+    if(!user) return res.status(400).send("El usuario no existe");
 
+    user = await User.findByIdAndUpdate(req.body._id, {
+        status: req.body.status
+    });
+    return res.status(200).send({user});
+});
 // exportar modulo
 module.exports = router;
 

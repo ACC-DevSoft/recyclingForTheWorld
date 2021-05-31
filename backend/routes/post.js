fe49048473b9b0e7f1e6d3c1be8ffc5c479841b1
Jalexss <jhalx2198@gmail.com>
2021-05-31 18:04:28 -0500

    Adding register post

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,8 +1,32 @@
+//* Express
 const express = require("express");
 const router = express.Router();
-const Auth = require("../middleware/auth");
+
+//* Models
 const User = require("../models/users");
 const Post = require("../models/post");
+
+const Auth = require("../middleware/auth");
+
+
+router.post("/addPost", Auth, async (req, res) => {
+    const user = await User.findById( req.user._id);
+
+    if(!user) return res.status(401).send("Usuario n autenticado");
+
+    const post = new Post({
+        userId: user._id,
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        status: true,
+    });
+
+    const savePost = await post.save();
+    return res.status(200).send({ savePost });
+});
+
+
 
 
 module.exports = router;

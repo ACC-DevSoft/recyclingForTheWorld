e5556c7eff474f6f6dc4a14445251f3f79b2a4c9
Davidhzk2 <dherrera035@misena.edu.co>
2021-05-31 08:43:58 -0500

    Estrcutura del proyecto, carpetas y librerias

---
+++ backend/models/users.js
@@ -0,0 +1,13 @@
+// importar los modulos utilizar 
+const mongoose = require("mongoose");
+const moment = require("moment");
+const jwt = require("jsonwebtoken");
+
+// logica 
+
+
+
+
+
+// exportar modelo
+module.exports = User;
---
+++ backend/routes/users.js
@@ -0,0 +1,11 @@
+// importar modulos
+const express = require("express");
+const router = express.Router();
+
+// logica
+
+
+// exportar modulo
+module.exports = router;
+
+

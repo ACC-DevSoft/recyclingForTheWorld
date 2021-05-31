7171e27d982e32628df97e889283a628c95a4203
Jalexss <jhalx2198@gmail.com>
2021-05-31 11:27:13 -0500

    fixing brunch jhon

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -54,23 +54,6 @@ router.put("/update", Auth, async (req, res) => {
 });
 
 // eliminar usuario
-<<<<<<< HEAD
-router.put("/cambiarStatus", Auth, async (req, res) => {
-	const user = await User.findById(req.user._id);
-	if (!user) return res.status(400).send("El usuario no existe");
-
-	const hash = await bcrypt.hash(req.body.password, 10);
-	user = await User.findByIdAndUpdate(req.body._id, {
-		rol: req.body.rol,
-		name: req.body.name,
-		lastName: req.body.lastName,
-		email: req.body.email,
-		password: hash,
-		phone: req.body.phone,
-		status: req.body.status,
-	});
-	return res.status(200).send({ user });
-=======
 router.put("/cambiarStatus", Auth, async(req, res) => {
     const user = await User.findById(req.user._id);
     if(!user) return res.status(400).send("El usuario no existe");
@@ -86,7 +69,6 @@ router.put("/cambiarStatus", Auth, async(req, res) => {
     status: req.body.status
     });
     return res.status(200).send({user});
->>>>>>> main
 });
 
 // exportar modulo

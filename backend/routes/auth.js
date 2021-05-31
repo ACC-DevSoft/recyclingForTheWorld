fe787b083faf58a195252e302d57b13dcbbde146
Davidhzk2 <dherrera035@misena.edu.co>
2021-05-30 22:14:33 -0500

    Autenticacion con login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,18 @@
 // importar modulos
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
 //Logica 
 router.post("/login/", async (req, res)=>{
+    const user = await User.findOne({username:req.body.username});
+    if(!user) return res.status(401).send({Message : "Usuario y/o contraseña incorrectos"});
 
+    const hash = await  bcrypt.compare(req.body.password, user.password);
+    if(!hash) return res.status(401).send({Message : "Usuario y/o contraseña incorrectos"});
+
+    const jwtToken = await User.generateToken();
+    return res.status(200).send({jwtToken});
 });
 
 // exportar modulo

6d6fed1c95adbfc358f82ede0dc186fec013b27f
diegodiazd10 <diegodiazd10@gmail.com>
2021-05-31 23:50:30 -0500

    Post Change Status

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -26,7 +26,27 @@ router.post("/addPost", Auth, async (req, res) => {
     return res.status(200).send({ savePost });
 });
 
-
-
+router.put("/changeStatus", Auth, async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if(!user) return res.status(401).send("Usuario no autenticado");
+    const post = await Post.findById(req.body._id);
+    if(!post) return res.status(401).send("El Post No existe")
+    let status = post.status;
+    if (status) {
+        status = false;
+    } else {
+        status = true;
+    } 
+    const postData = await Post.findByIdAndUpdate(req.body._id, {
+        _id: req.body._id,
+        userId: user._id,
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        status: status,
+    });
+    if(!post) return res.status(401).send("No se pudo Eliminar el post");
+    return res.status(200).send({post})
+})
 
 module.exports = router;

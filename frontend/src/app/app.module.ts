b3e510e1406656e1685574d0d1eca1746e647e99
Davidhzk2 <dherrera035@misena.edu.co>
2021-06-11 12:18:24 -0500

    frontend del register user

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AuthService } from './services/auth.service';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -44,6 +45,7 @@ import { HttpClientModule } from "@angular/common/http";
     HttpClientModule,
     FormsModule,
     MatListModule,
+    MatCardModule,
     ReactiveFormsModule
   ],
   providers: [AuthService],

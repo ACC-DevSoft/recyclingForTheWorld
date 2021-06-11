62a527d781e55f2282e32d4e89ac338be0b160a7
diegodiazd10 <diegodiazd10@gmail.com>
2021-06-11 12:29:46 -0500

    Implements Register Role

--- frontend/src/app/admin/register-role/register-role.component.ts
+++ frontend/src/app/admin/register-role/register-role.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-role',
   templateUrl: './register-role.component.html',
-  styleUrls: ['./register-role.component.css']
+  styleUrls: ['./register-role.component.css'],
 })
 export class RegisterRoleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public registerData: any;
+  public succesMessage: String;
+  public errorMessage: String;
+  constructor(private auth: AuthService, private router: Router) {
+    this.registerData = {};
+    this.succesMessage = '';
+    this.errorMessage = '';
   }
 
+  ngOnInit(): void {}
+
+  registerRole() {
+    if (!this.registerData.name || !this.registerData.description) {
+      console.log('Failed Process: Incomplete Data');
+      this.errorMessage = 'Failed Process: Incomplete Data';
+      this.closeAlert();
+      this.registerData = {};
+    } else {
+      this.auth.registerRole(this.registerData).subscribe(
+        (res) => {
+          console.log(res);
+          this.succesMessage = 'Register Role: successfull';
+          this.closeAlert();
+          this.registerData = {};
+        },
+        (err) => {
+          console.log(err);
+          this.errorMessage = err.error();
+          this.closeAlert();
+        }
+      );
+    }
+  }
+
+  closeAlert() {
+    setTimeout(() => {
+      this.succesMessage = '';
+      this.errorMessage = '';
+    }, 3000);
+  }
 }

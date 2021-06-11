2859426622c3aaee4f258b268e5940dbe32ee205
diegodiazd10 <diegodiazd10@gmail.com>
2021-06-11 09:58:55 -0500

    minor changes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './home/login/login.component';
 import { HeaderComponent } from './home/header/header.component';
 import { RegisterRoleComponent } from './admin/register-role/register-role.component';
 import { ListRoleComponent } from './admin/list-role/list-role.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ListRoleComponent } from './admin/list-role/list-role.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
